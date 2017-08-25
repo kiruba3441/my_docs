8646d8d757647bce8983e53fae22fd8d8b0975f7
Duraisamy <kduraisamy@Kiruba.local>
2017-08-25 07:43:14 +0530

    doc views

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,14 +1,24 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show,:edit,:update,:destroy]
   def index
+    @docs=Doc.all.order('created_at desc')
   end
 
   def show
+    @doc = Doc.find(params[:id])
   end
 
   def new
+    @doc = Doc.new
   end
 
   def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -23,9 +33,11 @@ class DocsController < ApplicationController
   private
 
   def find_doc
+    Doc.find(params[:id])
   end
 
   def doc_params
+    params.require(:doc).permit(:title,:content)
   end
 
 
