35b1bb7b647c74da59d348d8f8d03cd108bcd89c
Duraisamy <kduraisamy@Kiruba.local>
2017-08-25 08:07:10 +0530

    views for docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -5,7 +5,6 @@ class DocsController < ApplicationController
   end
 
   def show
-    @doc = Doc.find(params[:id])
   end
 
   def new
@@ -25,15 +24,22 @@ class DocsController < ApplicationController
   end
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
 
   def find_doc
-    Doc.find(params[:id])
+    @doc = Doc.find(params[:id])
   end
 
   def doc_params
